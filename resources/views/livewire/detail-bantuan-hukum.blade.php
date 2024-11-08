a44d3be89054e56fa3b123148cc719d0823356ef
hanafisg196 <hanafisg196@gmail.com>
2024-11-08 22:25:09 +0700

    jsjs

--- resources/views/livewire/detail-bantuan-hukum.blade.php
+++ resources/views/livewire/detail-bantuan-hukum.blade.php
@@ -44,11 +44,10 @@
                         <p class="text-bold-500">Email : {{ $data->email }}</p>
                         <p class="text-bold-500">Whatsapp : {{ $data->wa }}</p>
                         <p class="text-bold-500">Alamat : {{ $data->alamat }}</p>
-                        <p class="text-bold-500 mt-2">Kronologi :</p>
+                        <p class="text-bold-500">Kronologi :</p>
                         <p>
                             {{ $data->kronologi }}
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
-                        </p>
+
 
                     </div>
                     <h7 class="sidebar-label mt-2" style="margin-left: 10px;">
